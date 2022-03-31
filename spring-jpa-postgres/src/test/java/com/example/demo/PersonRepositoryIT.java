103b967788415dca60388f7b7e0397861e37e713
lofidewanto <lofidewanto@gmail.com>
2022-03-31 13:17:45 +0200

    update flyway

--- spring-jpa-postgres/src/test/java/com/example/demo/PersonRepositoryIT.java
+++ spring-jpa-postgres/src/test/java/com/example/demo/PersonRepositoryIT.java
@@ -10,7 +10,6 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
-import org.springframework.test.context.jdbc.Sql;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -22,34 +21,37 @@ class PersonRepositoryTest {
 
   @Container
   static PostgreSQLContainer database = new PostgreSQLContainer("postgres:12")
-    .withDatabaseName("springboot")
-    .withPassword("springboot")
-    .withUsername("springboot");
+      .withDatabaseName("springboot")
+      .withPassword("springboot")
+      .withUsername("springboot");
 
   @DynamicPropertySource
   static void setDatasourceProperties(DynamicPropertyRegistry propertyRegistry) {
     propertyRegistry.add("spring.datasource.url", database::getJdbcUrl);
     propertyRegistry.add("spring.datasource.password", database::getPassword);
     propertyRegistry.add("spring.datasource.username", database::getUsername);
+
+    propertyRegistry.add("spring.flyway.url", database::getJdbcUrl);
+    propertyRegistry.add("spring.flyway.password", database::getPassword);
+    propertyRegistry.add("spring.flyway.user", database::getUsername);
   }
 
   @Autowired
   private PersonRepository orderRepository;
 
   @Test
-  @Sql("/db/migration/V1__create_person_schema.sql")
   void shouldReturnPersons() {
     orderRepository.save(createPerson("Hans", 20));
 
     List<Person> persons = orderRepository.findAll();
 
-    assertEquals(1, persons.size());
+    assertEquals(2, persons.size());
   }
 
   private Person createPerson(String name, int age) {
     Person person = new Person();
-		person.setName("Hans");
-		person.setAge(42);
+    person.setName("Hans");
+    person.setAge(42);
     return person;
   }
 }
