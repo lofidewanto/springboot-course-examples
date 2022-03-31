b15c822ea226e999c9519234df9bc5727add2c94
lofidewanto <lofidewanto@gmail.com>
2022-03-31 09:02:49 +0200

    update pathVariable

--- spring-jpa-postgres/src/main/java/com/example/demo/PersonController.java
+++ spring-jpa-postgres/src/main/java/com/example/demo/PersonController.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,7 +18,7 @@ public class PersonController {
     }
     
     @GetMapping("/person/{id}")
-    public Person getPerson(Long id) {
+    public Person getPerson(@PathVariable Long id) {
         log.debug("Getting person with id: {}", id);
         
         return personService.getPerson(id);
