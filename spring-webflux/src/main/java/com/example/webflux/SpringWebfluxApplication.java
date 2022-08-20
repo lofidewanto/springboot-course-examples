89f8939ec756664ac5b07555a51c4cd705469119
lofidewanto <lofidewanto@gmail.com>
2022-08-20 18:02:13 +0200

    add Spring WebFlux

---
+++ spring-webflux/src/main/java/com/example/webflux/SpringWebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.example.webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebfluxApplication.class, args);
+	}
+
+}
