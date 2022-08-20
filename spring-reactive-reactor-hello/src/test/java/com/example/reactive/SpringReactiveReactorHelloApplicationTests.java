d3a0bdb1d7ba563324c0543b3f764162939eb00c
lofidewanto <lofidewanto@gmail.com>
2022-08-20 13:19:01 +0200

    update example

--- spring-reactive-reactor-hello/src/test/java/com/example/reactive/SpringReactiveReactorHelloApplicationTests.java
+++ spring-reactive-reactor-hello/src/test/java/com/example/reactive/SpringReactiveReactorHelloApplicationTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @SpringBootTest
 class SpringReactiveReactorHelloApplicationTests {
@@ -13,9 +14,20 @@ class SpringReactiveReactorHelloApplicationTests {
 	}
 
 	@Test
-	void test_Hello_World() {
+	void test_Hello_World_Flux() {
 		// Create a Flux that emits a sequence of random integers.
 		Flux<String> dataStream = Flux.just("Java", "C++", "Python", "Ruby");
+
+		// Subscribe to the Flux and print out each item.
+		dataStream.subscribe(prog -> {
+			System.out.println("Hello World " + prog);
+		});
+	}
+
+	@Test
+	void test_Hello_World_Mono() {
+		// Create a Flux that emits a sequence of random integers.
+		Mono<Integer> dataStream = Mono.just(1);
 
 		// Subscribe to the Flux and print out each item.
 		dataStream.subscribe(prog -> {
