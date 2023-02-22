c15da6a30cb4089b42245ba149a606637dc39809
lofidewanto <lofidewanto@gmail.com>
2023-02-22 19:19:08 +0100

    Update new Spring Boot

--- spring-aop-example/src/test/java/com/example/demo/server/SpringAopExampleApplicationTests.java
+++ spring-aop-example/src/test/java/com/example/demo/server/SpringAopExampleApplicationTests.java
@@ -1,16 +1,32 @@
 package com.example.demo.server;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@RunWith(SpringRunner.class)
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
 @SpringBootTest
 public class SpringAopExampleApplicationTests {
 
+	private static final Logger logger = LoggerFactory.getLogger(SpringAopExampleApplication.class);
+
+	@Autowired
+	Calculator calculator;
+
 	@Test
-	public void contextLoads() {
+	void contextLoads() {
+	}
+
+	@Test
+	void logging_aspect_test() {
+		int add = calculator.add(10, 20);
+
+		logger.info("Result: " + add);
+
+		assertEquals(30, add);
 	}
 
 }
