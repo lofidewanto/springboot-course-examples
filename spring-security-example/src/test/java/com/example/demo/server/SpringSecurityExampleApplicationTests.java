6842270f228df4e436c5ce1cf3606e87eacd9ea6
lofidewanto <lofidewanto@gmail.com>
2023-02-24 00:03:21 +0100

    add unit tests

--- spring-security-example/src/test/java/com/example/demo/server/SpringSecurityExampleApplicationTests.java
+++ spring-security-example/src/test/java/com/example/demo/server/SpringSecurityExampleApplicationTests.java
@@ -1,13 +1,35 @@
 package com.example.demo.server;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 public class SpringSecurityExampleApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	void hello_when_unauthenticated_then_redirect() throws Exception {
+		this.mockMvc.perform(get("/hello")).andExpect(status().is3xxRedirection());
+	}
+
+	@Test
+	@WithMockUser
+	void hello_when_authenticated_then_ok() throws Exception {
+		this.mockMvc.perform(get("/hello")).andExpect(status().isOk());
 	}
 
 }
