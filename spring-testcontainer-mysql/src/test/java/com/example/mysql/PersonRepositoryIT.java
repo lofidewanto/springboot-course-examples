e6679d2092067023611d13b53637aeeaf7500d5a
lofidewanto <lofidewanto@gmail.com>
2025-10-16 15:15:55 +0200

    Format code

--- spring-testcontainer-mysql/src/test/java/com/example/mysql/PersonRepositoryIT.java
+++ spring-testcontainer-mysql/src/test/java/com/example/mysql/PersonRepositoryIT.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MySQLContainer;
