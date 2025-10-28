afd4de4ca8341c041dfc550014e009ccbd0d8d6d
lofidewanto <lofidewanto@gmail.com>
2025-10-29 00:03:37 +0100

    Update Spring Boot 3.5.7

--- spring-schedule-async-cache-example/src/main/java/com/example/demo/server/BookRepository.java
+++ spring-schedule-async-cache-example/src/main/java/com/example/demo/server/BookRepository.java
@@ -1,7 +1,23 @@
 package com.example.demo.server;
 
-public interface BookRepository {
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Repository;
 
-	Book getByIsbn(String isbn);
+@Repository
+public class BookRepository {
+
+	@Cacheable("books")
+	public Book getByIsbn(String isbn) {
+		simulateSlowService();
+		return new Book(isbn, "Some book");
+	}
+
+	private void simulateSlowService() {
+		try {
+			Thread.sleep(8000L);
+		} catch (InterruptedException e) {
+			throw new IllegalStateException(e);
+		}
+	}
 
 }
